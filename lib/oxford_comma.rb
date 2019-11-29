afae4208aae8a62cf0029bfb8f09d67427836a05
celebrated-integer-2023 <scailles23@gmail.com>
2019-11-29 04:42:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
-if array.length<=2
+if array.size<=2
   return array.join(" and ")
   elsif array==3
   return array.join("test")
-else array.length>=4
+else array.size>=4
   return "hi"
 end
 end
