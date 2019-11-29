79da34ccc7678181bec2c15812bbe6ebe5509178
celebrated-integer-2023 <scailles23@gmail.com>
2019-11-29 06:09:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,3 @@
-def oxford_comma(array = nil)
-  return array.to_s if array.size==1
-  array[0..-2].join(", ") + " and " + array[-1]
-end
+def oxford_comma(array)
+  array=[]
+  return array.join if array.size==1
